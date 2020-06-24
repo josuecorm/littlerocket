761b35a519bddb29586302cd0e872c661757ce68
Josué Cornejo <josue.cornejo@wizeline.com>
2020-06-23 23:08:39 -0500

    Add earth css styles

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// Serve js files
+app.use('/js', express.static(path.join(__dirname, 'ui/js/')));
+// Serve css files
+app.use('/css', express.static(path.join(__dirname, 'ui/css/')));
+app.use('/img', express.static(path.join(__dirname, 'ui/img/')));
+
+
 app.get('/', (req, res) => {
-  res.send('Hello You!');
+    res.sendFile(path.join(__dirname + '/ui/index.html'));
 });
 
 app.listen(port, () => console.log(`App running on port ${port}`));
